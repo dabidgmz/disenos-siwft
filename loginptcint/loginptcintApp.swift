1e02491a9c98490cdfd6d80794e7337c9ef391bf
david <david.gmzherrera28@gmail.com>
2024-01-26 22:33:16 -0600

    Initial Commit

---
+++ loginptcint/loginptcintApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginptcintApp.swift
+//  loginptcint
+//
+//  Created by david gomez herrera on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct loginptcintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
